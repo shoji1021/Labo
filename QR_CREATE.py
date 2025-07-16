e60804e8ca51bcd73522b8f94e89b10e41440ecc
shoji1021 <shoji.masato@it-mirai-h.ibk.ed.jp>
2025-07-17 01:05:03 +0900

    pyfiglet追加

--- QR_CREATE.py
+++ QR_CREATE.py
@@ -1,4 +1,9 @@
 import qrcode
-url = input("リンクを入力してください:")
+import pyfiglet
+from colorama import Fore
+
+font = pyfiglet.figlet_format("QR Creator")
+print(Fore.BLUE+font)
+url = input(Fore.WHITE+"リンクを入力してください:")
 qr = qrcode.make(url)
 qr.show()
